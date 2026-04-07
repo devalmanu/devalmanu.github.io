cc35e0198d71421cd06fba3fa9449457bce963b1
devalmanu <manuilova-1986@mail.ru>
2026-04-07 17:19:44 +0300

    upgrade scripts and style fix

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.use(express.json());
+
+const TOKEN = `8612631410:AAFRV1PXbgLbSHd--QLGluUkNSN3Wwiko88`;
+const CHAT_ID = '493566464';
+
+app.post('/api/send', async (req,res)=>{
+  const {name,email,service,message} = req.body;
+
+  const text = `
+📩 Новая заявка
+
+👤 Имя: ${name}
+📧 Email: ${email}
+🛠 Услуга: ${service}
+💬 Сообщение: ${message}
+  `;
+
+  await axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+    chat_id: CHAT_ID,
+    text: text
+  });
+
+  res.send({ok:true});
+});
+
+app.listen(3000);
